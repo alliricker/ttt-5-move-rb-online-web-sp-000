8af88f6b3a3431cfab6f5a659104aae1b65e189c
Allison Ricker <alliricker@gmail.com>
2020-09-11 21:06:08 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -15,6 +15,14 @@ end
 board = [" ", " ", " "]
 
 
+def update_array_at_with(array, index, value)
+  array[index] = value
+end
+ 
+update_array_at_with(board, 0, "X")
+
+board 
+
 def move(board, index, character)
   board[index] = character
 end
