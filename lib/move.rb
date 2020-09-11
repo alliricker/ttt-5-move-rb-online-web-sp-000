8bba3c4f6d89d2a0ed85b1f3e520db83dab5d8fa
Allison Ricker <alliricker@gmail.com>
2020-09-11 19:29:32 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,6 +11,8 @@ def input_to_index(user_input)
   user_input.to_i-1
 end
 
-def move(board, index, player)
-  board[index] = player
+def move(board, index, value)
+  board[index] = value
 end
+
+move(board, O, "X")
